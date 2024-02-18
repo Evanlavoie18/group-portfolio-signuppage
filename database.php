60ad6a46f85b13ca607a2ad70913ae7c2fb67796
Evan Lavoie <151483214+Evanlavoie18@users.noreply.github.com>
2024-02-19 04:46:18 +0800

    Update database.php

--- database.php
+++ database.php
@@ -3,15 +3,9 @@
     $db_user = "root";
     $db_pass = "";
     $db_name = "signupdb";
-    $conn = "";
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name,);
 
-
-    try{
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    if ($conn->connect_error) { 
+        die("Connection failed: " . $conn->connect_error); 
     }
-    catch(mysqli_sql_exception){
-        echo"connection could not be established";
-    }
-
-    mysqli_close($conn);
 ?>
